3246d04146820f5fa8049b95daf6c1c694466f2c
beichenhpy <beichenhpy@outlook.com>
2021-01-27 22:12:22 +0800

    delete some useless files

--- src/main/java/cn/beichenhpy/mybatisplusdemo/service/IDeviceService.java
+++ src/main/java/cn/beichenhpy/mybatisplusdemo/service/IDeviceService.java
@@ -7,5 +7,9 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import java.util.List;
 
 public interface IDeviceService extends IService<Device> {
+    /**
+     * 模糊查询拼接分页
+     * @param searchList 分词集合
+     */
     void deviceList(List<String> searchList);
 }
